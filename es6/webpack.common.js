77810aaa9555e0397f1136f55827f11f8cf07232
Adrian <yanglinde@4399inc.com>
2019-08-27 20:09:30 +0800

    1

---
+++ es6/webpack.common.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+module.exports = {
+    entry : {
+        index : "./src/class.js",
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                  loader: "babel-loader"
+                }
+            }
+        ]
+    },
+    plugins: [
+        // This is especially useful for webpack bundles that include a hash in the filename
+        // which changes every compilation
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname,"index.tpl.html"),
+            inject:'body',
+            filename: './index.html'
+        }),
+        new CleanWebpackPlugin(['dist']),
+        new webpack.HotModuleReplacementPlugin()
+    ]
+};
